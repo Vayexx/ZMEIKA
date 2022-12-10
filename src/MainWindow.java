c18e2785d22848b253a5c83b3bb7f27e6c635615
ivanstrelkov <ivan_strelkov99@mail.ru>
2022-12-10 12:20:20 +0300

    init

---
+++ src/MainWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("ZMEIKA");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+
+    }
+
+
+}
